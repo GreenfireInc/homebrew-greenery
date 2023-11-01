681a2c4503ba4c74848afd8ac3ef81aff064a6a1
Timothy Reynolds <tmrdev@gmail.com>
2023-11-01 19:57:18 -0400

    v0_9_3 release with zap stanza that removes Greenery db along with plist Pref and savedState

--- Casks/greenery.rb
+++ Casks/greenery.rb
@@ -1,6 +1,6 @@
 cask "greenery" do
-  version "0.9.0"
-  sha256 :no_check 
+  version "0.9.3"
+  sha256 "cd2b9ae7dd26144d6147536500ce1ccaa2f8b0b5e20f023a7229615c37648fe1" 
 
   url "https://github.com/GreenfireInc/homebrew-greenery/releases/download/v#{version}/Greenery.zip"
   name "greenery"
@@ -8,4 +8,10 @@ cask "greenery" do
   homepage "https://github.com/GreenfireInc/homebrew-greenery/"
 
   app "Greenery.app"
+
+  zap trash: [
+    "~/Library/Application Support/Greenery",
+    "~/Library/Preferences/com.greenery.app.plist",
+    "~/Library/Saved Application State/com.greenery.app.savedState",
+  ]
 end
