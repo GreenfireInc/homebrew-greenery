80b83a36233e278d69cb5e221745a47e38102f04
Timothy Reynolds <tmrdev@gmail.com>
2023-11-27 14:24:12 -0500

    going to back to greenery from greenery-beta as v094 is the only stable version

--- Casks/greenery.rb
+++ Casks/greenery.rb
@@ -1,10 +1,10 @@
-cask "greenery-beta" do
+cask "greenery" do
   version "0.9.4"
   sha256 "13ae6e2a240f977c818f85a7a047c6e3d995a0407bc811affe0c4a6d9e9e3cc9"
 
   url "https://github.com/GreenfireInc/Releases.Greenery/releases/download/v#{version}/Greenery.#{version}.zip",
       verified: "github.com/GreenfireInc/Releases.Greenery/"
-  name "greenery-beta"
+  name "greenery"
   desc "Cryptocurrency bookkeeping and accounting wallet"
   homepage "https://www.greenery.finance/"
 
